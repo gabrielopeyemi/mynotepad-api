7036ee277c84d37e8dcf43374b772828607523d1
gabrielopeyemi <famosipe2010@gmail.com>
2021-11-25 12:31:28 +0100

    edit commit 001

--- src/main.ts
+++ src/main.ts
@@ -14,7 +14,6 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api-docs', app, document);
 
-
   await app.listen(9000);
 }
 bootstrap();
